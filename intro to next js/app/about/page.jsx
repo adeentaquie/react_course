fa525dd0485fe2fb435b440f4304ba514a934e49
adeen taquie <adeentaquie95@gmail.com>
2024-08-07 17:29:13 +0500

    intro to next js

---
+++ intro to next js/app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function AboutPage(){
+    return <main><h1>about us</h1></main>
+}
---
+++ intro to next js/app/blog/page.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+export default function BlogPage(){
+    return <main>
+        <h1>THe Blog</h1>
+        <p><Link href="/blog/post-1">Post1</Link></p>
+        <p><Link href="/blog/post-2">Post2</Link></p>
+
+    </main>
+}
